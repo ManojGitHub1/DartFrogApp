16eedfa03479165a3488c6ea35b7b068db5d47b4
ManojGitHub1 <manojjivanagi@gmail.com>
2024-11-09 20:20:36 +0530

    dartFrog.txt

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -1,6 +1,18 @@
 import 'package:dart_frog/dart_frog.dart';
 
+// calls function middleware
+// handler - handles request and returns handler
 Handler middleware(Handler handler) {
+  // we have other predefined middlewares and these are injucted in use method
+  // return handler.use(requestLogger()).use(authentication()).use(callsFunction());
+
+  // requestLogger() - Is a defined middleware which prints
+  // the time of request, request method, response status code and request folder(/items) from (localhost:8080/items)
+  // provider - dependency middleware a type of middleware
+  // accepts request context (here String) and returns a response context to a root handler
+  // In root handler context.read<String>();
+  // Therefore, we can use provider middleware to inject a value into the request context
+  // this is depentency injection
   return handler
       .use(requestLogger())
       .use(provider<String>((context) => 'Dart Frog'));
