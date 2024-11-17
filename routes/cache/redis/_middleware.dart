f8e62de47395ec412f1b36085b2a0c854fea8fef
ManojGitHub1 <manojjivanagi@gmail.com>
2024-11-17 17:02:18 +0530

    dartfrog

--- routes/cache/redis/_middleware.dart
+++ routes/cache/redis/_middleware.dart
@@ -1,9 +1,39 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:redis/redis.dart';
 
 // Outside Handler are private variable
-String? greeting; // using as temporary cache
+// String? greeting; // using as temporary cache
+
+final conn = RedisConnection();
 
 Handler middleware(Handler handler) {
-  greeting = 'Hi';
-  return handler.use(provider<String>((context) => greeting ??= 'Hello'));
+  // greeting = 'Hi';
+  // return handler.use(provider<String>((context) => greeting ??= 'Hello'));
+
+  return (context) async {
+    Response response;
+
+    // host port:8091 and redis port: 6379
+    try {
+      final command = await conn.connect('localhost', 8091);
+
+      try {
+        await command.send_object(
+          ['AUTH', 'default', 'Manojredis123'],
+        );
+
+        response =
+            await handler.use(provider<Command>((_) => command)).call(context);
+      } catch (e) {
+        response = Response.json(
+          body: {'success': false, 'message': e.toString()},
+        );
+      }
+    } catch (e) {
+      response = Response.json(
+          body: {'success': false, 'message': 'Failed to connect to Redis'});
+    }
+
+    return response;
+  };
 }
--- routes/cache/redis/index.dart
+++ routes/cache/redis/index.dart
@@ -1,6 +1,50 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  final value = context.read<String>();
-  return Response(body: value);
+import 'package:dart_frog/dart_frog.dart';
+import 'package:redis/redis.dart';
+
+// 1-logged In , 0-Logged Out
+
+Future<Response> onRequest(RequestContext context) async {
+  // final value = context.read<String>();
+  // return Response(body: value);
+
+  return switch (context.request.method) {
+    HttpMethod.get => _getLoginStatus(context),
+    HttpMethod.post => _setLoginStatus(context),
+    _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed))
+  };
+}
+
+Future<Response> _getLoginStatus(RequestContext context) async {
+  final value = await context.read<Command>().send_object(
+    ['GET', 'loggedIn'],
+  ).then((value) => value);
+
+  if (value == null) {
+    const status = 0;
+    await context.read<Command>().send_object(
+      ['SET', 'loggedIn', status],
+    );
+    return Response(statusCode: HttpStatus.noContent);
+  } else {
+    return Response.json(body: value.toString());
+  }
+}
+
+Future<Response> _setLoginStatus(RequestContext context) async {
+  final body = await context.request.json() as Map<String, dynamic>;
+  final status = body['loggedIn'] as int?;
+  var success = false;
+
+  try {
+    await context.read<Command>().send_object(
+      ['SET', 'loggedIn', status],
+    );
+    success = true;
+  } catch (e) {
+    success = false;
+  }
+
+  return Response.json(body: {'success': success});
 }
