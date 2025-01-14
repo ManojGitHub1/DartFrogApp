ccd2fb2c76736e517ff7a5d58f5853637eb22dbb
ManojGitHub1 <manojjivanagi@gmail.com>
2025-01-15 01:21:13 +0530

    dartfrog

--- routes/api/restapi/index.dart
+++ routes/api/restapi/index.dart
@@ -1,5 +1,25 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  return Response(body: 'This is a new route!');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:http/http.dart' as http;
+
+Future<Response> onRequest(RequestContext context) {
+  return switch (context.request.method) {
+    HttpMethod.get => _getRecipes(context),
+    _ => Future.value(Response(statusCode: HttpStatus.methodNotAllowed)),
+  };
+}
+
+Future<Response> _getRecipes(RequestContext context) async {
+  final response = await http.get(
+    Uri.parse('https://google.com'),
+    headers: {'X-RapidAPI-Key': '', 'X-RapidAPI-Host': ''},
+  );
+
+  if (response.statusCode == 200) {
+    return Response.json(body: response.body);
+  } else {
+    return Response(
+        statusCode: HttpStatus.badGateway, body: 'Failed to retrieve recipes');
+  }
 }
